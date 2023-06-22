28965d8df8f5d93bf6a4da235cadbce1ad8a79d5
nickolanack <nickblackwell82@gmail.com>
2023-06-22 11:49:23 -0700

    add script to compile all markdown into json array

---
+++ survey/web/current/markdown/markdown.php
@@ -0,0 +1,15 @@
+<?php
+
+$dir='../0fcf27faad/templates/';
+
+$data=[];
+
+foreach(array_values(array_filter(scandir($dir), function($file){
+    return strpos($file, '.md')!==false;
+})) as $file){
+
+    $data[str_replace('.md', '', $file)]=file_get_contents($dir.$file);
+
+}
+
+echo json_encode(["markdown"=>$data], JSON_PRETTY_PRINT);
